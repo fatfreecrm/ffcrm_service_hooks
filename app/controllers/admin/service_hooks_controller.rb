f9577b4e72c0c391da5186eb58243d8511f1a5f8
Steve Kenworthy <steveyken@gmail.com>
2012-01-03 11:56:43 +0800

    Added configurable api service hooks admin settings.

--- app/controllers/admin/service_hooks_controller.rb
+++ app/controllers/admin/service_hooks_controller.rb
@@ -6,7 +6,7 @@ class Admin::ServiceHooksController < Admin::ApplicationController
   #----------------------------------------------------------------------------
   def index
     @service_hooks = Setting[:service_hooks] ||= {}
-
+    @service_hooks_url = set_service_hooks_url
     respond_to do |format|
       format.html # index.html.haml
     end
@@ -16,11 +16,23 @@ class Admin::ServiceHooksController < Admin::ApplicationController
   #----------------------------------------------------------------------------
   def update
     Setting[:service_hooks] = params[:service_hooks]
+    @service_hooks_url = set_service_hooks_url
     flash[:notice] = 'Service Hooks saved'
 
     respond_to do |format|
       format.js # update.js.rjs
     end
   end
-end
 
+protected
+
+  def set_service_hooks_url
+    if Setting[:service_hooks][:merge_url].blank?
+      "Undefined - please enter details above"
+    else
+      "POST #{Setting[:service_hooks][:merge_url]}" +
+      (Setting[:service_hooks][:api_token].blank? ? '' : "?api_token=#{Setting[:service_hooks][:api_token]}")
+    end
+  end
+
+end
--- lib/crm_service_hooks/models.rb
+++ lib/crm_service_hooks/models.rb
@@ -1,16 +1,20 @@
 Contact.class_eval do
   def merge_with_with_service_hook(master, ignored_attr = {})
     if merge_with_without_service_hook(master, ignored_attr)
-      if merge_url = (Setting[:service_hooks] || {})["merge_url"]
+      merge_url = (Setting[:service_hooks] || {})["merge_url"]
+      api_token = (Setting[:service_hooks] || {})["api_token"]
+      if merge_url
+        params = {:format => :form,
+                  :merge => {
+                    :old_contact => {:id => self.id},
+                    :new_contact => {:id => master.id, :name => master.name}
+                  },
+                 }
+        params.merge!(:api_token => api_token) if api_token.present?
         Rails.logger.info "Contact merge service hook: POST data to #{merge_url}..."
+        Rails.logger.debug "Params: #{params.to_s}"
         begin
-          Nestful.post merge_url, :format => :form,
-                                  :params => {
-                                    :merge => {
-                                      :old_contact => {:id => self.id},
-                                      :new_contact => {:id => master.id, :name => master.name}
-                                    }
-                                  }
+          Nestful.post merge_url, params
         rescue Exception => ex
           Rails.logger.error "POST failed! #{ex.message}"
         end
