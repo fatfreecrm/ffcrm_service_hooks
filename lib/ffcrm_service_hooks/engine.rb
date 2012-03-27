7a4a589438a61b6e6ff8e37a51ada02ea1a7af0b
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-27 16:13:40 +0800

    Rescue TypeError if settings table hasn't been migrated

--- lib/ffcrm_service_hooks/engine.rb
+++ lib/ffcrm_service_hooks/engine.rb
@@ -2,13 +2,17 @@ module FatFreeCRM::ServiceHooks
   class Engine < Rails::Engine
     config.to_prepare do
       require 'ffcrm_service_hooks/contact'
-    
+
       if FatFreeCRM::Tabs.admin.present?
 
-        FatFreeCRM::Tabs.admin << {
-          :text => "Service Hooks",
-          :url => { :controller => "admin/service_hooks" }
-        }
+        begin
+          FatFreeCRM::Tabs.admin << {
+            :text => "Service Hooks",
+            :url => { :controller => "admin/service_hooks" }
+          }
+        rescue TypeError
+          puts "You must migrate your settings table."
+        end
 
         if Setting[:service_hooks].blank?
           puts "Please configure your service hook settings on the admin tab."
