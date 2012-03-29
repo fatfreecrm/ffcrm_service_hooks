760a9af612745a0131a71450230fd3343d04e81e
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-29 12:28:04 +0800

    FatFreeCRM::Tabs.admin.present? is no longer a necessary test

--- lib/ffcrm_service_hooks/engine.rb
+++ lib/ffcrm_service_hooks/engine.rb
@@ -4,20 +4,17 @@ module FatFreeCRM
       config.to_prepare do
         require 'ffcrm_service_hooks/contact'
 
-        if FatFreeCRM::Tabs.admin.present?
+        begin
+          FatFreeCRM::Tabs.admin << {
+            :text => "Service Hooks",
+            :url => { :controller => "admin/service_hooks" }
+          }
+        rescue TypeError
+          puts "You must migrate your settings table."
+        end
 
-          begin
-            FatFreeCRM::Tabs.admin << {
-              :text => "Service Hooks",
-              :url => { :controller => "admin/service_hooks" }
-            }
-          rescue TypeError
-            puts "You must migrate your settings table."
-          end
-
-          if Setting[:service_hooks].blank?
-            puts "Please configure your service hook settings on the admin tab."
-          end
+        if Setting[:service_hooks].blank?
+          puts "Please configure your service hook settings on the admin tab."
         end
       end
     end
